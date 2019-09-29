8b71cb8ec202bfc28cafd28e8fc05c3bf4af5412
guojunhang <1364020251@qq.com>
2019-09-29 17:43:49 +0800

    first commit

---
+++ src/views/Info.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      hello info component
+      <button @click="add()">添加</button>
+    </div>
+</template>
+
+
+<script>
+    import store from '@/store'
+    export default {
+        name: "Info",
+        store,
+        data(){
+            return{
+              msg: '国庆节'
+            }
+        },
+        methods:{
+            add(){
+                console.log('add Event from info!')
+                debugger
+                store.commit('increase')
+            },
+            output () {
+                console.log('in output')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
